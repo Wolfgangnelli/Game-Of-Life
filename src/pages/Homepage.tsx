7db299d110066cd2472cc99cc8fd4bc51d431112
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-05-30 14:41:37 +0200

    game description

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -8,6 +8,9 @@ const Homepage = () => {
     <div className='py-10'>
       <h1 className='mb-6 font-bold text-5xl text-white'>{homePageTitle}</h1>
       <img src={frontImage} alt="conway's game of life" className='mx-auto mb-5' />
+      <div className='mb-6 text-white'>
+        <p>This is a zero-player game and its evolution is determined by its initial state, requiring no further input.</p>
+      </div>
       <Button label="GET STARTED" icon={<i className="fa-solid fa-arrow-right-long"></i>} href="/game" />
     </div>
   )
