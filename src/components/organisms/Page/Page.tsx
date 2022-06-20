374475801d6982d8bb146d22b9b2a5294b47a6ee
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-06-20 10:01:38 +0200

    added css roles at page component

--- src/components/organisms/Page/Page.tsx
+++ src/components/organisms/Page/Page.tsx
@@ -9,13 +9,13 @@ const Page = (props: Props) => {
     const {children = null} = props
 
   return (
-    <>
+    <div className='h-screen w-full'>
         <Header />
-        <main className='max-w-7xl mx-auto h-screen'>
+        <main className='max-w-7xl mx-auto h-auto'>
             {children}
         </main>
         <Footer />
-    </>
+    </div>
   )
 }
 
