e526b17b431e80924ceb0cfbf1f0e6520b98399e
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-06-20 10:01:55 +0200

    fix footer

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='py-5 bottom-0 text-white'>
-        <p>Game Of Life</p>
+    <footer className='py-5 px-2 w-full h-20 bottom-0 absolute text-white flex items-center justify-center'>
+     <p> Game Of Life </p>
     </footer>
   )
 }
